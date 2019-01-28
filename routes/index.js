ddd3f60e5773763451affafe052dd61c3a1fe919
gaofei <xxoommd@163.com>
2019-01-28 20:52:55 +0800

    Init project

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const fs = require('fs')
+
+const validExt = ['.txt', '.epub', '.mobi', '.pdf', '.doc', '.docx']
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let files = fs.readdirSync(path.join(global.__rootdir, 'public', 'books'))
+
+  files = files.filter(book => validExt.includes(path.extname(book)))
+
+  const list = files.map(book => {
+    return {
+      name: path.basename(book),
+      link: `/books/${encodeURIComponent(book)}`
+    }
+  })
+
+  let data = {
+    title: 'Kindle Share',
+    books: list
+  }
+
+  res.render('index', data);
+});
+
+module.exports = router;
